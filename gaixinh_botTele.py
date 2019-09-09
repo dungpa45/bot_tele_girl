870374474bf79a1dfd21b14be6a0289f30c5874e
Dung-dev <osam.dungpham@gmail.com>
2019-09-09 09:24:25 +0700

    reflactor code

--- gaixinh_botTele.py
+++ gaixinh_botTele.py
@@ -7,52 +7,35 @@ markup = ReplyKeyboardMarkup(reply_keboard,one_time_keyboard=True)
 
 CHOOSE = 1
 
-def get_girl_img():
-    img_dir = "F:\ANH\Girls\GaiChauA"
+def get_image_local(img_dir):
     data_path = os.path.join(img_dir,'*.jpg')
     files = glob.glob(data_path)
     img_ = random.choice(files)
     img = img_.replace('\\','/')
+    return img
+
+def get_girl_img():
+    img = get_image_local("F:\ANH\Girls\GaiChauA")
     return img
 
 def get_woman_img():
-    img_dir = "F:\ANH\Girls\GaiXinhChonLoc"
-    data_path = os.path.join(img_dir,'*.jpg')
-    files = glob.glob(data_path)
-    img_ = random.choice(files)
-    img = img_.replace('\\','/')
+    img = get_image_local("F:\ANH\Girls\GaiXinhChonLoc")
     return img
 
 def get_vsbg_img():
-    img_dir = "F:\ANH\Girls\VSBG"
-    data_path = os.path.join(img_dir,'*.jpg')
-    files = glob.glob(data_path)
-    img_ = random.choice(files)
-    img = img_.replace('\\','/')
+    img = get_image_local("F:\ANH\Girls\VSBG")
     return img
 
 def get_korea_img():
-    img_dir = "F:\ANH\Girls\Korea"
-    data_path = os.path.join(img_dir,'*.jpg')
-    files = glob.glob(data_path)
-    img_ = random.choice(files)
-    img = img_.replace('\\','/')
+    img = get_image_local("F:\ANH\Girls\Korea")
     return img
 
 def get_gaitay_img():
-    img_dir = "F:\ANH\Girls\GaiTay"
-    data_path = os.path.join(img_dir,'*.jpg')
-    files = glob.glob(data_path)
-    img_ = random.choice(files)
-    img = img_.replace('\\','/')
+    img = get_image_local("F:\ANH\Girls\GaiTay")
     return img
 
 def get_twice_img():
-    img_dir = "F:\ANH\Girls\Twice"
-    data_path = os.path.join(img_dir,'*.jpg')
-    files = glob.glob(data_path)
-    img_ = random.choice(files)
-    img = img_.replace('\\','/')
+    img = get_image_local("F:\ANH\Girls\Twice")
     return img
 
 def start(bot,update):
@@ -61,65 +44,61 @@ def start(bot,update):
         "Hello master {} \nType /gái /sexygirl /girl /lady /vsbg /woman /korean /gaitay to see random picture\nHave fun :)".format(user.full_name),
         reply_markup=markup)
 
+def help(bot,update):
+    update.message.reply_text('''Note:
+    /gái /girl /lady /woman: Gái xinh chọn lọc
+    /sexygirl /vsbg : Gái xinh quyến rũ
+    /korean /korea /gáihàn: Gái Hàn Xẻng
+    /gaitay /gáitây: Gái Tây
+    Have fun :)''')
+
 def girl(bot,update):
     girl = get_girl_img()
-    #Get the recipient’s ID
     chat_id = update.message.chat_id
     mess_id = update.message.message_id
-    #it’s time to send the message, which is an image.
     bot.send_photo(chat_id=chat_id,reply_to_message_id=mess_id, photo=open(girl,"rb"))
 
 def woman(bot,update):
     woman = get_woman_img()
-    #Get the recipient’s ID
     chat_id = update.message.chat_id
     mess_id = update.message.message_id
-    #it’s time to send the message, which is an image.
     bot.send_photo(chat_id=chat_id,reply_to_message_id=mess_id, photo=open(woman,"rb"))
     
 def vsbg(bot,update):
     vsbg = get_vsbg_img()
-    #Get the recipient’s ID
     chat_id = update.message.chat_id
     mess_id = update.message.message_id
-    #it’s time to send the message, which is an image.
     bot.send_photo(chat_id=chat_id,reply_to_message_id=mess_id, photo=open(vsbg,"rb"))
 
 def korea(bot,update):
     korea = get_korea_img()
-    #Get the recipient’s ID
     chat_id = update.message.chat_id
     mess_id = update.message.message_id
-    #it’s time to send the message, which is an image.
     bot.send_photo(chat_id=chat_id,reply_to_message_id=mess_id, photo=open(korea,"rb"))
 
 def gaitay(bot,update):
     gaitay = get_gaitay_img()
-    #Get the recipient’s ID
     chat_id = update.message.chat_id
     mess_id = update.message.message_id
-    #it’s time to send the message, which is an image.
     bot.send_photo(chat_id=chat_id,reply_to_message_id=mess_id, photo=open(gaitay,"rb"))
 
 def twice(bot,update):
     twice = get_twice_img()
-    #Get the recipient’s ID
     chat_id = update.message.chat_id
     mess_id = update.message.message_id
-    #it’s time to send the message, which is an image.
     bot.send_photo(chat_id=chat_id,reply_to_message_id=mess_id, photo=open(twice,"rb"))
 
 def main():
-    TOKEN = "876374897:AAGL2K9-sUb_7mnVso8WddzlYn69_9bIsHg"
-    # PORT = int(os.environ.get('PORT', '8443'))
+    TOKEN = "token"
     updater = Updater(TOKEN)
     
     dp = updater.dispatcher
     start_handler = CommandHandler('start',start)
+    dp.add_handler(CommandHandler('help',help))
     dp.add_handler(CommandHandler(['girl','gái'],girl))
     dp.add_handler(CommandHandler(['woman','gái','lady'],woman))
     dp.add_handler(CommandHandler(['vsbg','sexygirl','sexylady'],vsbg))
-    dp.add_handler(CommandHandler(['korea','korean'],korea))
+    dp.add_handler(CommandHandler(['korea','korean','gaihan'],korea))
     dp.add_handler(CommandHandler(['gáitây','gaitay'],gaitay))
     dp.add_handler(CommandHandler(['long','twice'],twice))
     dp.add_handler(start_handler)
